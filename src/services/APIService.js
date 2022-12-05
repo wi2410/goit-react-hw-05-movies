6b464d072c6930eca482c1fc357194d4706d2d60
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-12-06 00:56:14 +0200

    fix bugs

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -6,41 +6,35 @@ import axios from 'axios';
 const API_KEY = 'f6e581dc0b1e4310711b9bd2dd0db6b6';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
-const END_POINTS = {
-  trending: '/trending/movie/week',
-  searchMovie: '/search/movie',
-  details: '/movie',
-};
-
 export const getPopularMovie = async () => {
-  const response = await axios.get(`${END_POINTS.trending}?api_key=${API_KEY}`);
+  const response = await axios.get(`${'/trending/movie/week'}?api_key=${API_KEY}`);
   return response.data.results;
 };
 
 export const searchMovie = async query => {
   const response = await axios.get(
-    `${END_POINTS.searchMovie}?api_key=${API_KEY}&query=${query}`
+    `${'/search/movie'}?api_key=${API_KEY}&query=${query}`
   );
   return response.data.results;
 };
 
 export const getDetails = async id => {
   const response = await axios.get(
-    `${END_POINTS.details}/${id}?api_key=${API_KEY}`
+    `${'/movie'}/${id}?api_key=${API_KEY}`
   );
   return response.data;
 };
 
 export const getCredits = async id => {
   const response = await axios.get(
-    `${END_POINTS.details}/${id}/credits?api_key=${API_KEY}`
+    `${'/movie'}/${id}/credits?api_key=${API_KEY}`
   );
   return response.data;
 };
 
 export const getReviews = async id => {
   const response = await axios.get(
-    `${END_POINTS.details}/${id}/reviews?api_key=${API_KEY}`
+    `${'/movie'}/${id}/reviews?api_key=${API_KEY}`
   );
   return response.data;
 };
